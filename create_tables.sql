7211b46b6c4d7782e61d405552fffd89b272a736
Semen Oskolkov <oskolkovsemen@yandex.ru>
2023-01-11 02:20:39 +0600

    File with command for create tables in PostgreSQL

---
+++ create_tables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE employees
+(
+	employee_id SERIAL PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes varchar(450) NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100) NOT NULL
+)
